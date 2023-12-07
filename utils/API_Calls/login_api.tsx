8677a09ccc87b3c4f2b620222f978eecc81faabb
its-me-yps <yps8181@gmail.com>
2023-12-07 23:55:06 +0530

    handle_Logout

--- utils/API_Calls/login_api.tsx
+++ utils/API_Calls/login_api.tsx
@@ -1,5 +1,5 @@
 import {SHA256, Decryption_AES} from "../Encryption"
-import {Set_PrivK, Set_PubK, Set_Data, Set_Gender, Set_Claims, Set_Submit} from "../UserData"
+import {Set_PrivK, Set_PubK, Set_Data, Set_Gender, Set_Claims, Set_Submit, Set_Id} from "../UserData"
 import { fetchAndDecodeHearts } from "./recievedHearts"
 import { returnHearts_Late } from "./returnHearts"
 import { FetchReturnedHearts } from "./Matching"
@@ -7,11 +7,12 @@ const SERVER_IP = process.env.SERVER_IP
 
 export const handleLog = async(data: any) => {
     try {
+      Set_Id(data.id);
       const passHash = await SHA256(data.password)
       const res = await fetch(
           `${SERVER_IP}/session/login`, {
               method: "POST",
-              // credentials: "include",  // uncomment this line if server running on same host as frontend (CORS)
+              credentials: "include",  // uncomment this line if server running on same host as frontend (CORS)
               body: JSON.stringify({
                   _id: data.id,
                   passHash: passHash
@@ -42,7 +43,7 @@ export const handleLog = async(data: any) => {
         await returnHearts_Late()
       }
       
-      await FetchReturnedHearts()
+      // await FetchReturnedHearts()
 
       return true
     }
@@ -51,3 +52,21 @@ export const handleLog = async(data: any) => {
       return false
     }
   }
+
+export const handle_Logout = async() => {
+  try {
+    const res = await fetch(
+      `${SERVER_IP}/session/logout`, {
+        method: "GET"
+      }
+    )
+    if (!res.ok) {
+      throw new Error(`HTTP Error: ${res.status} - ${res.statusText}`);
+    }
+    return true;
+  }
+  catch(err) {
+    console.log(err)
+    return false 
+  }
+}
